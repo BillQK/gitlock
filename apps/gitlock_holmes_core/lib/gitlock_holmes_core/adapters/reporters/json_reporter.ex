e83eded89d211ad57e1e5001d28b99229658f140
Khoi Ngo <ngo.kho@northeastern.edu>
2025-05-03 14:05:45 -0400

    json rows

--- apps/gitlock_holmes_core/lib/gitlock_holmes_core/adapters/reporters/json_reporter.ex
+++ apps/gitlock_holmes_core/lib/gitlock_holmes_core/adapters/reporters/json_reporter.ex
@@ -6,12 +6,13 @@ defmodule GitlockHolmesCore.Adapters.Reporters.JsonReporter do
 
   @impl true
   @spec report(results :: [map()], opts :: map()) :: {:ok, String.t()} | {:error, String.t()}
-  def report(results, _opts) do
+  def report(results, opts) do
     results =
       Enum.map(results, fn
         %{__struct__: _} = struct -> Map.from_struct(struct)
         map -> map
       end)
+      |> Enum.take(opts.rows)
 
     {:ok, Jason.encode!(results, pretty: true)}
   end
