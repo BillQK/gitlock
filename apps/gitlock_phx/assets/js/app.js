f838a12d3e7fe2945c13aa3be74801c2ae37e4f3
Khoi Ngo <67892332+BillQK@users.noreply.github.com>
2025-06-11 11:26:14 -0400

    feat: detect mode

--- apps/gitlock_phx/assets/js/app.js
+++ apps/gitlock_phx/assets/js/app.js
@@ -23,6 +23,7 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
+import "./theme.js";
 
 let Hooks = {};
 
